1828097711597bf066508de9f6823c24345a65dc
Weizhen <wangwzhen@gmail.com>
2019-04-26 20:26:36 +0000

    get data from api

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,11 +1,12 @@
 let request = require("request");
 
-request("http://www.google.com", function(error, response, body) {
+request("https://jsonplaceholder.typicode.com/users", function(error, response, body) {
     if(error) {
         console.log("something is wrong");
         console.log(error);
     } else if(!error && response.statusCode == 200) {
-        console.log(body);
+        let parsedData = JSON.parse(body);
+        console.log(parsedData[1]["address"]["city"]);
     } else {
         console.log(response.statusCode);
     }
