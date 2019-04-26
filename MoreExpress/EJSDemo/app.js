b6f8c3ce6699ac5e5813433f0bbeb998e53841e4
Weizhen <wangwzhen@gmail.com>
2019-04-26 16:16:27 +0000

    add external stylesheet and partials folder with header/footer

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,9 +1,12 @@
 let express = require("express");
 let app = express();
 
+app.use(express.static("public"));//set public folder as the place to hold the stylesheet and js file
+app.set("view engine", ".ejs"); //set ejs file as default file type
+
 //home page
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 //city page - using res.render and variables
@@ -11,7 +14,7 @@ app.get("/travel/:continent/:city", function(req, res) {
     let continent = req.params.continent;
     let city = req.params.city;
     
-    res.render("city.ejs", {
+    res.render("city", {
                     contiName: continent, 
                     cityName: city
                     });
@@ -25,7 +28,7 @@ app.get("/capitals", function(req, res) {
         {country: "Germany", capital: "Berlin"},
         {country: "Italy", capital: "Rome"}
     ]
-    res.render("capitals.ejs", {capitalObj: capitals});
+    res.render("capitals", {capitalObj: capitals});
 });
 
 //start server
