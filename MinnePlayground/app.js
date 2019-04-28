b399d94f7c3e462ab69c52bfc1fd09d4115b8bd6
Weizhen <wangwzhen@gmail.com>
2019-04-28 03:52:14 +0000

    post route added

--- MinnePlayground/app.js
+++ MinnePlayground/app.js
@@ -1,9 +1,17 @@
 let express = require("express");
 let app = express ();
 
-app.set("view engine", "ejs");
+let playgrounds =[
+        {name: "InnerActive", image: "https://thriftyminnesota.com/wp-content/uploads/2016/10/InnerActive-indoor-playground-1.jpg"},
+        {name: "Williston Treehouse", image: "https://eminnetonka.com/images/williston/treehouse/IMG_0084a.jpg"},
+        {name: "Ridgedale center play area", image: "https://3h40go1k7x4w3f2n2mbwapbf-wpengine.netdna-ssl.com/wp-content/uploads/2-adc-architecture-design-collaborative-retail-renovation-ridgedale-play-center-minnetonka-mn.jpg"},
+        {name: "Mini-Hops Gymnastics", image: "https://i.ytimg.com/vi/lDsyp_rKnLk/maxresdefault.jpg"}
+        ];
 
+app.set("view engine", "ejs"); //set default file type
 
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); //require body-parser
 
 
 //landing page
@@ -11,11 +19,24 @@ app.get("/", function(req, res) {
     res.render("landing");
 })
 
-//playgrounds
+//playgrounds page
 app.get("/playgrounds", function(req, res) {
-    res.render("playgrounds");
+    res.render("playgrounds", {playgrounds: playgrounds});
 })
 
+//POST route
+app.post("/playgrounds", function(req, res) {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newPlayground = {name: name, image: image};
+    playgrounds.push(newPlayground);
+    res.redirect("/playgrounds");
+})
+
+//create new playground
+app.get("/playgrounds/new", function(req, res) {
+    res.render("new");
+})
 //start server
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("server started");
